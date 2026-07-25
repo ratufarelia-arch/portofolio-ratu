2687010a5c9958658c6090fbb03ccafef42166f0
ratufarelia <ratufarelia@gmail.com>
2026-07-25 16:09:39 +0700

    clean up setup with view compiled path

--- api/index.php
+++ api/index.php
@@ -1,30 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Buat folder storage di /tmp Vercel
-$storagePath = '/tmp/storage';
-if (!is_dir($storagePath)) {
-    mkdir($storagePath . '/framework/views', 0755, true);
-    mkdir($storagePath . '/framework/cache/data', 0755, true);
-    mkdir($storagePath . '/framework/sessions', 0755, true);
-    mkdir($storagePath . '/logs', 0755, true);
-}
-
-putenv("APP_STORAGE={$storagePath}");
-$_ENV['APP_STORAGE'] = $storagePath;
-
-// Autoload & Booting
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Bind path storage secara benar ke instance Application
-$app->useStoragePath($storagePath);
-
-// Handle request
-$request = Request::capture();
-$response = $app->handle($request);
-$response->send();
+require __DIR__ . '/../public/index.php';
